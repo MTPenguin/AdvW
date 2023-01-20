20534e66892b63fb556439a0dd1e84393240b423
unknown <MTPenguin>
2023-01-20 13:45:46 -0700

    Add V2 migrations

---
+++ migrations/U002_20230120204034__BMac.sql
@@ -0,0 +1,56 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Altering [dbo].[ufnGetCustomerInformation]'
+GO
+ALTER FUNCTION [dbo].[ufnGetCustomerInformation](@CustomerID int)
+RETURNS TABLE 
+AS 
+-- Returns the CustomerID, first name, and last name for the specified customer.
+RETURN (
+    SELECT 
+        CustomerID, 
+        FirstName, 
+        LastName
+    FROM [SalesLT].[Customer] 
+    WHERE [CustomerID] = @CustomerID
+);
+GO
+PRINT N'Altering [dbo].[ufnGetAllCategories]'
+GO
+-- DROP FUNCTION [dbo].[ufnGetAllCategories]
+
+ALTER FUNCTION [dbo].[ufnGetAllCategories]()
+RETURNS @retCategoryInformation TABLE 
+(
+    -- Columns returned by the function
+    [ParentProductCategoryName] [nvarchar](50) NULL, 
+    [ProductCategoryName] [nvarchar](50) NOT NULL,
+	[ProductCategoryID] [int] NOT NULL
+)
+AS 
+-- Returns the CustomerID, first name, and last name for the specified customer.
+BEGIN
+	WITH CategoryCTE([ParentProductCategoryID], [ProductCategoryID], [Name]) AS 
+	(
+		SELECT [ParentProductCategoryID], [ProductCategoryID], [Name]
+		FROM SalesLT.ProductCategory
+		WHERE ParentProductCategoryID IS NULL
+
+	UNION ALL
+
+		SELECT C.[ParentProductCategoryID], C.[ProductCategoryID], C.[Name]
+		FROM SalesLT.ProductCategory AS C
+		INNER JOIN CategoryCTE AS BC ON BC.ProductCategoryID = C.ParentProductCategoryID
+	)
+
+	INSERT INTO @retCategoryInformation
+	SELECT PC.[Name] AS [ParentProductCategoryName], CCTE.[Name] as [ProductCategoryName], CCTE.[ProductCategoryID]  
+	FROM CategoryCTE AS CCTE
+	JOIN SalesLT.ProductCategory AS PC 
+	ON PC.[ProductCategoryID] = CCTE.[ParentProductCategoryID];
+	RETURN;
+END;
+GO
+
